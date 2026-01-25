25768887780f04e39c5f75dc3c848424f63ce9ed
jean toneli <jeantoneli@gmail.com>
2026-01-25 23:17:51 +0000

    Add status column to recipes

--- db/migrate/20260125225952_add_status_to_recipes.rb
+++ db/migrate/20260125225952_add_status_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddStatusToRecipes < ActiveRecord::Migration[7.1]
   def change
-    add_column :recipes, :status, :string
+    add_column :recipes, :status, :string, default: 'pending'
   end
 end
